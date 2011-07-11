dd0b29661480bb53696ab69f7df335a329e3e15a
thomas <gr@grrrr.org>
2011-07-11 09:10:41 +0000

    setup script
    git-svn-id: https://svn.grrrr.org/projects/dev/nsgt@797 3d3ad2b0-3bb8-11dd-8da9-e1ab7d56d6ab

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name = "NSGT",
+    version = "0.1",
+    author = "Thomas Grill",
+    author_email = "gr@grrrr.org",
+    description = ("Python implementation of Nonstationary Gabor transform"),
+    license = "GPL",
+    keywords = "fourier gabor",
+    url = "http://grrrr.org",
+    packages=['nsgt'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python"
+    ],
+)
