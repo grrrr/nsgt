ceab3c69c2354ce6bb0911abd9d29a95807e608f
thomas <gr@grrrr.org>
2011-07-28 12:53:01 +0000

    with plotting
    git-svn-id: https://svn.grrrr.org/projects/dev/nsgt@831 3d3ad2b0-3bb8-11dd-8da9-e1ab7d56d6ab

--- examples/transform_audio.py
+++ examples/transform_audio.py
@@ -13,6 +13,19 @@ from nsgt import CQ_NSGT
 from scikits.audiolab import Sndfile
 from time import time
 import os.path
+from itertools import imap
+
+class interpolate:
+    def __init__(self,cqt,Ls):
+        from scipy.interpolate import interp1d
+        self.intp = [interp1d(N.linspace(0,Ls,len(r)),r) for r in cqt]
+    def __call__(self,x):
+        try:
+            len(x)
+        except:
+            return N.array([i(x) for i in self.intp])
+        else:
+            return N.array([[i(xi) for i in self.intp] for xi in x])
 
 if __name__ == "__main__":    
     from optparse import OptionParser
@@ -21,6 +34,7 @@ if __name__ == "__main__":
     parser.add_option("--fmin",dest="fmin",type="float",default=80,help="minimum frequency")
     parser.add_option("--fmax",dest="fmax",type="float",default=22050,help="maximum frequency")
     parser.add_option("--bins",dest="bins",type="int",default=12,help="bins per octave")
+    parser.add_option("--plot",dest="plot",type="int",default=0,help="plot transform (needs installed matplotlib and scipy packages)")
     
     (options, args) = parser.parse_args()
     if not len(args):
@@ -43,7 +57,7 @@ if __name__ == "__main__":
     
     # forward transform 
     c = nsgt.forward(s)
-    
+
     # inverse transform 
     s_r = nsgt.backward(c)
     
@@ -53,3 +67,13 @@ if __name__ == "__main__":
     rec_err = norm(s-s_r)/norm(s)
     print "Reconstruction error: %.3e"%rec_err
     print "Calculation time: %.3f s"%(t2-t1)
+
+    if options.plot:
+        import pylab as P
+        # interpolate CQT to get a grid
+        x = N.linspace(0,Ls,1000)
+        grid = interpolate(imap(N.abs,c[2:len(c)/2]),Ls)(x)
+        # display grid
+        P.imshow(N.log(N.flipud(grid.T)),aspect=2)
+        print "Plotting"
+        P.show()
